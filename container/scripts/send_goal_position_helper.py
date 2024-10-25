9133f4a40dd2a69cff5bac0331a18278b0241f98
Henrik Lurz <henrik.lurz@web.de>
2024-10-25 17:53:52 +0200

    Removed unnecessary code from multi launch python script

--- container/scripts/send_goal_position_helper.py
+++ container/scripts/send_goal_position_helper.py
@@ -1,19 +1,14 @@
 import os
 
-from asyncio import sleep
-from signal import SIGINT, SIGKILL
+from signal import SIGINT
 import subprocess
 
 if __name__ == "__main__":
     for x in range(5):
         print("Start run: " + str(x))
         launch_process = subprocess.Popen(["ros2", "launch", "gpp_pipeline", "pipeline.launch.py"], text=True)
-        # print("Wait Start ---------------------------------------------------------------")
-        launch_process.wait()
-        # print("Sleep Start ---------------------------------------------------------------")
-        # sleep(20)
-        print("Process End ---------------------------------------------------------------")
-        launch_process.send_signal(SIGINT) # Stop launch file
+        launch_process.wait() # Wait for launch file to be killed
+        # launch_process.send_signal(SIGINT) # Stop launch file. Dont know if still necessary?
         os.system("kill $(ps aux | grep 'ign gazebo gui' | awk '{print $2}')") # Kill Gazebo Ingition explicit
         launch_process.wait(timeout=30)
         print("End run: " + str(x))
