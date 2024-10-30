1f04795c1901f2a06b8fd651eb097bae81aea05b
Henrik Lurz <henrik.lurz@web.de>
2024-10-30 22:34:11 +0100

    Removed unnecessary code

--- gpp_pipeline/gpp_pipeline/create_position_list_node.py
+++ gpp_pipeline/gpp_pipeline/create_position_list_node.py
@@ -157,79 +157,14 @@ def main(args = None):
 
     create_position_list_node: CreatePositionListNode = CreatePositionListNode()
 
-    # Publisher will only be called once. It needs time to be ready.
-    # sleep(1)
-
-    # send_new_goal_node.send_goal()
-
-    # Pause the programm until its killed. All tasks still will be processed in the background.
-    # rclpy.spin(send_new_goal_node)
-
-    # Destroy the node explicitly, otherwise garbage collector should do it.
-    # send_new_goal_node.destroy_node()
-
     # rclpy.spin(create_position_list_node)
     create_position_list_node.run()
+
+    # Destroy the node explicitly, otherwise garbage collector should do it.
+    create_position_list_node.destroy_node()
 
     # Stops all communication and should always be called last in a node before finishing.
     rclpy.shutdown()
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-# class PipelineConfig:
-#     map_name: str
-#     map_package: str
-
-#     robot_launch_file: str
-#     robot_launch_package: str
-
-#     number_of_tests: int
-
-#     robot_spawn_position_x: float
-#     robot_spawn_position_y: float
-#     robot_spawn_orientation_yaw: float
-
-#     robot_target_position_x: float
-#     robot_target_position_y: float
-#     robot_target_orientation_yaw: float
-
-#     def __init__(self) -> None:
-#         pass
-    
-#     def import_config(self) -> None:
-
-#         # Get directory the script is in
-#         file_dir: str = os.path.dirname(os.path.realpath(__file__))
-#         # Get file path but with /../ in the middle
-#         file_name: str = os.path.join(file_dir, '../config/config.yaml')
-#         # Get absolut path without /../ in the middle
-#         config_path:str = os.path.abspath(os.path.realpath(file_name))
-
-#         with open(config_path, 'r') as file:
-#             config_data = yaml.safe_load(file)
-            
-#             self.map_name = config_data["map_name"]
-#             self.map_package = config_data["map_package"]
-
-#             self.robot_launch_file = config_data["robot_launch_file"]
-#             self.robot_launch_package = config_data["robot_launch_package"]
-
-#             self.number_of_tests = config_data["number_of_tests"]
-
-#             self.robot_spawn_position_x = config_data["robot_spawn_position_x"]
-#             self.robot_spawn_position_y = config_data["robot_spawn_position_y"]
-#             self.robot_spawn_orientation_yaw = config_data["robot_spawn_orientation_yaw"]
-
-#             self.robot_target_position_x = config_data["robot_target_position_x"]
-#             self.robot_target_position_y = config_data["robot_target_position_y"]
-#             self.robot_target_orientation_yaw = config_data["robot_target_orientation_yaw"]
-
-
-
-# if __name__=="__main__":
-#     pass
