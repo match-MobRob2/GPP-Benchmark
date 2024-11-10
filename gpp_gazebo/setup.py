de450c7b86ce2101ed221456c2b3565beb667823
Henrik Lurz <henrik.lurz@web.de>
2024-11-10 15:21:58 +0100

    Fixed missing laujnch copy

--- gpp_gazebo/setup.py
+++ gpp_gazebo/setup.py
@@ -11,6 +11,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
         (os.path.join("share", package_name, "worlds"), glob("worlds/*.sdf")),
         (os.path.join("share", package_name, "worlds/meshes"), glob("worlds/meshes/*.obj")),
         (os.path.join("share", package_name, "maps"), glob("maps/*.yaml")),
